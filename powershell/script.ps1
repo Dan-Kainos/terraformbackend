e564a856fae22b143e8cbc9c5dc938005a43353a
Daniel Rainbow <daniel.rainbow@kainos.com>
2023-02-06 08:59:33 +0000

    fixed comments

--- powershell/script.ps1
+++ powershell/script.ps1
@@ -8,34 +8,25 @@ $ctx = $storageAccount.Context
 $containerName = "tfstate"
 $vaultName = "alzdevopskv"
 
-# Resource group creation
+# create resource group
 New-AzResourceGroup -Name $resourceGroup -Location $location
-# Storage account creation
+
+# create storage account
 $storageAccount = New-AzStorageAccount -ResourceGroupName $resourceGroup `
     -Name $accountName `
     -Location $location `
     -SkuName $storageSku `
     -Kind $storageKind
-# Storage container creation
+
+# create container
 New-AzStorageContainer -Name $containerName -Context $ctx -Permission blob
-# Key vault creation
+
+# create keyvault
 New-AzKeyVault -VaultName $vaultName -ResourceGroupName $resourceGroup -Location $location
 
 # Environment variable for terraform backend
-# To configure backend state you need the following information
-# storage account name
-# container name
-# key - name of state file
-# access_key - storage access key
-# The following command stores access key as env variable
 $ACCOUNT_KEY=(Get-AzStorageAccountKey -ResourceGroupName $resourceGroup -Name $accountName)[0].value
 $env:ARM_ACCESS_KEY=$ACCOUNT_KEY
 
-##### REMOVE RESOURCE GROUP #############
+## remove resource group
 # Remove-AzResourceGroup -Name $resourceGroup
-#-----------------------------------------
-# Get Az Subscription
-# Get Az RG
-# Create Storage account and Container
-# Storage for Terraform remote state
-# Create KV for secrets
